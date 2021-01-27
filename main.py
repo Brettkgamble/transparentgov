61238ca6086a98da30acab4df6094de1771de34e
brettkgamble@gmail.com <5413CrossFit2018>
2021-01-27 10:15:58 -0700

    0004 - Database Setup

--- main.py
+++ main.py
@@ -9,71 +9,120 @@ from sqlalchemy.orm import sessionmaker
 import psycopg2
 from psycopg2 import Error
 
-# from flask import Flask, jsonify, request
-# from flask_cors import cross_origin, CORS
-# from models.models import Base
+from flask import Flask, jsonify, request
+from flask_cors import cross_origin, CORS
+from models.models import Base
 # from endpointClasses.resources import Resources
 #
-# from sqlalchemy import Column, Integer, BigInteger, String, Text, DateTime, \
-#     Float, Boolean, func, ForeignKeyConstraint, Index, ForeignKey
-# from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import relationship, backref
-# from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
-# from datetime import datetime, timedelta, timezone
-# from geoalchemy2 import Geography, Geometry
+from sqlalchemy import Column, Integer, BigInteger, String, Text, DateTime, \
+    Float, Boolean, func, ForeignKeyConstraint, Index, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
+from datetime import datetime, timedelta, timezone
+from geoalchemy2 import Geography, Geometry
+
+### Setup the application
+app = Flask(__name__)
+
+# Wrap CORS around the app so that the server does not block machine to machine
+# or browser based requests
+CORS(app)
+
+
 
 def main():
+
     print('In Main')
+
     # engine below for local PostgresSql access
-    try:
-        # connect to existing database - localhost
-        connection = psycopg2.connect(user="postgres",
-                                      password="5413CrossFit2018",
-                                      host="localhost",
-                                      port="5433",
-                                      database="postgres")
-        # Create a cursor to perform database operations
-        cursor = connection.cursor()
-        # Print PosthreSQL details
-        print("PostgreSQL information")
-        print(connection.get_dsn_parameters(), "\n")
-        # Executing a SQL query
-        cursor.execute("SELECT version();")
-        # Fetch Result
-        record = cursor.fetchone()
-        print("You are connected to - ", record, "\n")
 
-    except (Exception, Error) as error:
-        print("Error while connecting to PostgreSQL", error)
-
-    finally:
-        if (connection):
-            cursor.close()
-            connection.close()
-            print("PostgreSQL connection is closed")
-
-    # # Engine below for Google Cloud PostgreSQL access.
-    # engine = create_engine(
-    #     'postgresql+psycopg2://postgres:5413CrossFit2018@35.232.127.36/test')
+    # try:
+    #     # connect to existing database - localhost
+    #     connection = psycopg2.connect(user="postgres",
+    #                                   password="5413CrossFit2018",
+    #                                   host="localhost",
+    #                                   port="5433",
+    #                                   database="postgres")
+    #     # Create a cursor to perform database operations
+    #     cursor = connection.cursor()
+    #     # Print PosthreSQL details
+    #     print("PostgreSQL information")
+    #     print(connection.get_dsn_parameters(), "\n")
+    #     # Executing a SQL query
+    #     cursor.execute("SELECT version();")
+    #     # Fetch Result
+    #     record = cursor.fetchone()
+    #     print("You are connected to - ", record, "\n")
     #
-    # Session = sessionmaker(bind=engine)
-    # session = Session()
+    # except (Exception, Error) as error:
+    #     print("Error while connecting to PostgreSQL", error)
+    #
+    # finally:
+    #     if (connection):
+    #         cursor.close()
+    #         connection.close()
+    #         print("PostgreSQL connection is closed")
+
+
+
+    # engine below for GAE PostgresSql access
+
+    # try:
+    #     # connect to existing database - localhost
+    #     connection = psycopg2.connect(user="postgres",
+    #                                   password="5413CrossFit2018",
+    #                                   host="34.70.40.80",
+    #                                   port="5432",
+    #                                   database="transgov")
+    #     # Create a cursor to perform database operations
+    #     cursor = connection.cursor()
+    #     # Print PosthreSQL details
+    #     print("PostgreSQL information")
+    #     print(connection.get_dsn_parameters(), "\n")
+    #     # Executing a SQL query
+    #     cursor.execute("SELECT version();")
+    #     # Fetch Result
+    #     record = cursor.fetchone()
+    #     print("You are connected to - ", record, "\n")
+    # except (Exception, Error) as error:
+    #     print("Error while connecting to PostgreSQL", error)
+    #
+    # finally:
+    #     if (connection):
+    #         cursor.close()
+    #         connection.close()
+    #         print("PostgreSQL connection is closed")
+
+        # Another way to connect to GAE PostgreSQL - Connection string can be
+        # placed in the yaml file and read from there
+
+    # Engine below for Google Cloud PostgreSQL access.
+    engine = create_engine('postgresql+psycopg2://postgres:5413CrossFit2018@34'
+                           '.70.40.80/transgov')
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
 
     # download  csv file
     print('Downloading .....')
-    col_names = ["Ministry", "Position", "Name", "Type", "Date", "Amount",
-             "Description", "Receipt 1", "Receipt 2", "Receipt 3"]
-    df = pd.read_csv(r'https://expenses.alberta.ca/DownloadData.aspx?type=csv'
-                   r'&d=IsVE/OcdpNZJ5rBbvji3qw', names=col_names,
-                     low_memory=False)
 
-    # Saving the dataframe
-    df.to_csv('expenses.csv')
-
-    print(df.head())
+    # col_names = ["Ministry", "Position", "Name", "Type", "Date", "Amount",
+    #          "Description", "Receipt 1", "Receipt 2", "Receipt 3"]
+    # df = pd.read_csv(r'https://expenses.alberta.ca/DownloadData.aspx?type=csv'
+    #                r'&d=IsVE/OcdpNZJ5rBbvji3qw', names=col_names,
+    #                  low_memory=False)
+    #
+    # # Saving the dataframe
+    # df.to_csv('expenses.csv')
+    #
+    # print(df.head())
 
     # Save to expenses table
+    print('Saving csv locally .....')
     # Identify dups.  If duplicate then add to duplicate table
     # if record already exists then simply update the lastseen field with
     # current date
---
+++ models/models.py
@@ -0,0 +1,52 @@
+from sqlalchemy import Column, Integer, BigInteger, String, Text, \
+    Date, DateTime, Float, Boolean, func, ForeignKeyConstraint, Index, \
+    ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.hybrid import hybrid_property, hybrid_method
+from datetime import date, datetime, timedelta, timezone
+from geoalchemy2 import Geography, Geometry
+
+import os
+import json
+
+
+Base = declarative_base()
+
+class ExpensesRaw(Base):
+    """
+    Raw Expenses Table
+    """
+    __tablename__ = 'expensesraw'
+
+    id = Column(Integer(), autoincrement=True, primary_key=True)
+    ministry = Column(String())
+    position = Column(String())
+    name = Column(String())
+    type = Column(String())
+    expense_date = Column(DateTime())
+    amount = Column(Float())
+    description = Column(String())
+    receipt1 = Column(String())
+    receipt2 = Column(String())
+    receipt3 = Column(String())
+    date_last_found = Column(DateTime(), default=datetime.now)
+    created_at = Column(DateTime(), default=datetime.now)
+    updated_at = Column(DateTime(), default=datetime.now,
+                        onupdate=datetime.now)
+
+    def __repr__(self):
+        return "ExpensesRaw(id='{self.id}' , " \
+            "ministry='{self.ministry}' , " \
+            "position='{self.position}' , " \
+            "name='{self.name}' , " \
+            "type='{self.type}' , " \
+            "expense_date='{self.expense_date}' , " \
+           "amount='{self.amount}' , " \
+           "description='{self.description}' , " \
+            "receipt1='{self.receipt1}' , "\
+            "receipt2='{self.receipt2}' , " \
+            "receipt3='{self.receipt3}' , " \
+            "date_last_found='{self.date_last_found}' , " \
+               "created_at='{self.created_at}' , " \
+               "updated_at='{self.updated_at}')".format(self=self)
