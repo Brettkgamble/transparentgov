6e33564e51d0131673c8974414632eb14696288b
brettkgamble@gmail.com <5413CrossFit2018>
2021-02-07 11:12:32 -0700

    0013 - Updated Model

--- models/models.py
+++ models/models.py
@@ -31,7 +31,7 @@ class ExpensesRaw(Base):
     receipt1 = Column(String())
     receipt2 = Column(String())
     receipt3 = Column(String())
-    is_duplicate = Column(Boolean(), default=False)
+    is_duplicated = Column(Boolean(), default=False)
     date_last_found = Column(DateTime(), default=datetime.now)
     created_at = Column(DateTime(), default=datetime.now)
     updated_at = Column(DateTime(), default=datetime.now,
@@ -49,7 +49,7 @@ class ExpensesRaw(Base):
             "receipt1='{self.receipt1}' , "\
             "receipt2='{self.receipt2}' , " \
             "receipt3='{self.receipt3}' , " \
-               "is_duplicate='{self.is_duplicate}' , " \
+               "is_duplicated='{self.is_duplicate}' , " \
                "date_last_found='{self.date_last_found}' , " \
                "created_at='{self.created_at}' , " \
                "updated_at='{self.updated_at}')".format(self=self)
