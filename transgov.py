4ebf672d0afa74d2ba185cdc580448b8c5851217
brettkgamble@gmail.com <5413CrossFit2018>
2021-02-24 08:33:10 -0700

    0023 - Building py file

--- transgov.py
+++ transgov.py
@@ -35,3 +35,39 @@ app = Flask(__name__)
 # Wrap CORS around the app so that the server does not block machine to machine
 # or browser based requests
 CORS(app)
+
+# Engine below for Google Cloud PostgreSQL access.
+engine = create_engine('postgresql+psycopg2://postgres:5413CrossFit2018@34'
+                       '.70.40.80/transgov')
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Column Names of each column in the dataset
+col_names = ["Ministry", "Position", "Name", "Type", "Category", "Date", "Amount",
+         "Description", "Receipt 1", "Receipt 2", "Receipt 3"]
+
+def getExpensesFromSource():
+    # download  csv file
+    # Only do this once per day
+    print('Downloading .....')
+    df = pd.read_csv(r'https://expenses.alberta.ca/DownloadData.aspx?type=csv'
+                     r'&d=IsVE/OcdpNZJ5rBbvji3qw', names=col_names,
+                     low_memory=False, parse_dates=['Date'])
+    # Saving the dataframe
+    print('Saving to CSV ...')
+    filename = 'expenses/' + str(time.strftime('%Y%m%d')) + ".csv"
+    df.to_csv(filename)
+    print('done...')
+
+    return filename
+
+def loadDfFromFile(filename):
+    # We do this because we want to skip the heading rows
+    print('Reading from file...', filename)
+    # filename = "/" + filename
+    df=pd.read_csv(filename, names=col_names, low_memory=False, skiprows=2)
+    df_size = len(df.index)
+    print('Done...', df_size)
