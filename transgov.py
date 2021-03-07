744fb68f1f319a74b34d7a910572a08e01b6a259
brettkgamble@gmail.com <5413CrossFit2018>
2021-03-07 10:05:01 -0700

    0028 - Building py file

--- transgov.py
+++ transgov.py
@@ -137,3 +137,71 @@ def mergeAndSaveDataframeDiff(df_diff_df):
     df_diff_df._merge.unique()
     filename = 'expenses/' + 'diff_' + str(time.strftime('%Y%m%d')) + ".csv"
     df_diff_df.to_csv(filename)
+
+def addNewDataToExpensesRaw(df_diff_df):
+    # Add left_only to the table expensesraw and update the 'changed' field to true for any that are right_only
+    start_time = time.time()
+    length = len(df_diff_df)
+    ctr = 0
+
+    for index, row in df_diff_df.iterrows():
+
+        newdate = df_diff_df._get_value(index, 'Date')
+        newdate = datetime.strptime(newdate, '%m/%d/%Y')
+        newAmount = df_diff_df._get_value(index, 'Amount').replace('$',
+                                                                   '').replace(
+            ',', '')
+        newAmount = float(newAmount)
+
+        if len(str(df_diff_df._get_value(index, 'Receipt 1'))) > 5:
+            newReceipt1 = df_diff_df._get_value(index, 'Receipt 1')
+        else:
+            newReceipt1 = ''
+        if len(str(df_diff_df._get_value(index, 'Receipt 2'))) > 5:
+            newReceipt2 = df_diff_df._get_value(index, 'Receipt 2')
+        else:
+            newReceipt2 = ''
+        if len(str(df_diff_df._get_value(index, 'Receipt 3'))) > 5:
+            newReceipt3 = df_diff_df._get_value(index, 'Receipt 3')
+        else:
+            newReceipt3 = ''
+
+        if df_diff_df._get_value(index, '_merge') == 'left_only':
+            changed = False
+
+        if df_diff_df._get_value(index, '_merge') == 'right_only':
+            changed = True
+
+        expense = ExpensesRaw(
+            ministry=df_diff_df._get_value(index, 'Ministry'),
+            position=df_diff_df._get_value(index, 'Position'),
+            name=df_diff_df._get_value(index, 'Name'),
+            type=df_diff_df._get_value(index, 'Type'),
+            category=df_diff_df._get_value(index, 'Category'),
+            expense_date=newdate,
+            amount=newAmount,
+            description=df_diff_df._get_value(index, 'Description'),
+            receipt1=newReceipt1,
+            receipt2=newReceipt2,
+            receipt3=newReceipt3,
+            changed=changed,
+            date_last_found=datetime.now(),
+            created_at=datetime.now(),
+            updated_at=datetime.now(),
+        )
+
+        if ctr % 100 == 0:
+            perc = "{:.3f}".format(ctr / length)
+            elapsed = "{:.2f}".format(time.time() - start_time)
+            print('Count: %s of %s percentage %s elapsed %s ' % (
+            ctr, length, perc, elapsed))
+
+        ctr = ctr + 1
+
+        try:
+            session.add(expense)
+            session.commit()
+        except SQLAlchemyError as e:
+            session.rollback()
+            print('Could not save the new expense %s',
+                  expense)
